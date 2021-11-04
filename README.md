1ea23e827a20fc89dea1265c7ff4e81decea406f
cglotr <amir.ariffin.1992@gmail.com>
2021-11-05 02:23:45 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Arctic Tern
+[![Build Status](https://app.travis-ci.com/arikama/go-arctic-tern.svg?branch=master)](https://app.travis-ci.com/arikama/go-arctic-tern)
+[![codecov](https://codecov.io/gh/arikama/go-arctic-tern/branch/master/graph/badge.svg?token=xTbyaIEFCN)](https://codecov.io/gh/arikama/go-arctic-tern)
+
+![Arctic Tern](./arctic_tern.jpeg)
+
+[Arctic Tern](https://en.wikipedia.org/wiki/Arctic_tern#:~:text=The%20Arctic%20tern%20is%20famous%20for%20its%20migration) is a small MySQL migration library for [Golang](https://golang.org/).
--- arctictern/migrate_test.go
+++ arctictern/migrate_test.go
@@ -11,13 +11,25 @@ func TestMigrate(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	migrationDir := "./../migrationexample"
+	migrationDir := "./../migration/example"
 	err = Migrate(db, migrationDir)
 	if err != nil {
 		panic(err)
 	}
 	err = Migrate(db, migrationDir)
 	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMigrateInvalid(t *testing.T) {
+	db, err := mysqltestcontainer.Start("test", "")
+	if err != nil {
+		panic(err)
+	}
+	migrationDir := "./../migration/invalid"
+	err = Migrate(db, migrationDir)
+	if err != nil && err.Error() != "Error 1064: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'INVALID' at line 1" {
 		panic(err)
 	}
 }
