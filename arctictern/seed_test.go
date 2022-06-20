de97e85f58e6a3d63bcf4bc901efd20c3e2d5544
cglotr <amir.ariffin.1992@gmail.com>
2022-06-20 15:46:54 +0800

    assert insert count

--- arctictern/seed_test.go
+++ arctictern/seed_test.go
@@ -5,30 +5,35 @@ import (
 
 	"github.com/arikama/go-arctic-tern/arctictern"
 	"github.com/arikama/go-mysql-test-container/mysqltestcontainer"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSeed(t *testing.T) {
 	result, err := mysqltestcontainer.Start("test")
-	if err != nil {
-		panic(err)
-	}
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+
 	db := result.Db
 	migrationDir := "./../migration/example"
 	err = arctictern.Migrate(db, migrationDir)
-	if err != nil {
-		panic(err)
-	}
+	assert.Nil(t, err)
+
 	err = arctictern.Migrate(db, migrationDir)
-	if err != nil {
-		panic(err)
-	}
+	assert.Nil(t, err)
+
 	seedDir := "./../seed/example"
 	err = arctictern.Seed(db, seedDir)
-	if err != nil {
-		panic(err)
-	}
+	assert.Nil(t, err)
+
 	err = arctictern.Seed(db, seedDir)
-	if err != nil {
-		panic(err)
+	assert.Nil(t, err)
+
+	rows, err := db.Query(`SELECT * FROM user;`)
+	assert.Nil(t, err)
+
+	count := 0
+	for rows.Next() {
+		count += 1
 	}
+	assert.Equal(t, 3, count)
 }
